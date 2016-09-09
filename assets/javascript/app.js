8b65224c8344ed053af00b08aa521545bf0d2a0d
MTibby92 <MTibby92@gmail.com>
2016-09-08 21:17:35 -0500

    wrote 3 ajax calls to lastfm; return relevant responses in console on search

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,3 +10,77 @@ var sharedSecret = 'd1727a270c67dc265f0b9d9b4910ffc9'
 
 // WORKING EXAMPLE LASTFM API CALL
 // http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=Linkin+Park&api_key=26a76686375358b55dd7488f7bf1256d&format=json
+
+var queryString = 'http://ws.audioscrobbler.com/2.0'
+
+function artistGetInfo() {
+    var searchInput = $('#searchInput').val().trim()
+    // $('#searchInput').val('')
+    var searchData = {
+        method: "artist.getinfo",
+        artist: searchInput,
+        autocorrect: 1,
+        api_key: apiKey,
+        format: "json"
+    };
+    var results
+
+    $.ajax({url: queryString, method: 'GET', data: searchData})
+    .done(function(response) {
+        console.log('Get Info response: ')
+        console.log(response)
+        console.log('The artist name is:  ' + response.artist.name) // Artist name
+        console.log('Here is the image link: ' + response.artist.image[4]["#text"]) // Artist mega image (biggest size) index 3 is smaller
+
+        results = response.artist
+    })
+}
+
+function artistGetTopTracks() {
+    var searchInput = $('#searchInput').val().trim()
+    // $('#searchInput').val('')
+    var searchData = {
+        method: "artist.getTopTracks",
+        artist: searchInput,
+        autocorrect: 1,
+        limit: 5,
+        api_key: apiKey,
+        format: "json"
+    };
+    var results
+
+    $.ajax({url: queryString, method: 'GET', data: searchData})
+    .done(function(response) {
+        console.log('Top Tracks response: ')
+        console.log(response)
+    })
+}
+
+function artistGetTopAlbums() {
+    var searchInput = $('#searchInput').val().trim()
+    // $('#searchInput').val('')
+    var searchData = {
+        method: "artist.getTopAlbums",
+        artist: searchInput,
+        autocorrect: 1,
+        limit: 3,
+        api_key: apiKey,
+        format: "json"
+    };
+    var results
+
+    $.ajax({url: queryString, method: 'GET', data: searchData})
+    .done(function(response) {
+        console.log('Top Albums response: ')
+        console.log(response)
+    })
+}
+
+$( document ).ready(function() {
+    $('#search').on('click', function(event) {
+    	artistGetInfo()
+    	artistGetTopTracks()
+    	artistGetTopAlbums()
+    	$('#searchInput').val('')
+    })
+})
