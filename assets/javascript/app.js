836c0979003a3fb0a1241cdcf3c7c94d1745fbb2
MTibby92 <MTibby92@gmail.com>
2016-09-08 20:11:59 -0500

    added api info to app.js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+// LastFM API key
+var apiKey = '26a76686375358b55dd7488f7bf1256d'
+var sharedSecret = 'd1727a270c67dc265f0b9d9b4910ffc9'
+// registered to MTibby92
+// app name is "Bootcamp Artist & Concert Recommendation App"
+
+// Song Kick API key
+// applied 9/8, can take up to 7 days to get a key
